68f0639e67ce7b52b76c0d690e65f58e8d7e7481
Elizaveta <eremeonock.el@gmail.com>
2023-09-13 17:22:38 +0300

    Initial Commit

---
+++ work_hard_suai/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  work_hard_suai
+//
+//  Created by Elizaveta Eremyonok on 13.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
